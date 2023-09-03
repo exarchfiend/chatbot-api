0be55e084e45f6709de90fdc7a028c0642781c54
MJ <gunnarlarsen3y@gmail.com>
2023-09-03 07:39:39 -0700

    MJ,feat:init ddd project

---
+++ chatbot-api-interfaces/src/test/java/fun/mjauto/chatbot/api/ApiTest.java
@@ -0,0 +1,11 @@
+package fun.mjauto.chatbot.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ApiTest {
+    @Test
+    void contextLoads() {
+    }
+}
